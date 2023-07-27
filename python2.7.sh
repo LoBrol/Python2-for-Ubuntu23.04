c2430f6faff3a18ee182216ae256f081a984ea6a
LoBrol <67478509+LoBrol@users.noreply.github.com>
2023-07-27 21:06:52 +0200

    Update python2.7.sh

--- python2.7.sh
+++ python2.7.sh
@@ -6,9 +6,9 @@ wget https://www.python.org/ftp/python/2.7.9/Python-2.7.9.tgz
 tar xzf Python-2.7.9.tgz
 cd Python-2.7.9
 ./configure --enable-optimizations --includedir=/usr/include/tirpc/
-make altinstall
+make
+sudo make install
 
 
-# cd ~
-# echo alias python2='python2.7' >> .bashrc
-# source ~/.bashrc
+sudo ln -s /usr/local/bin/python2.7 /usr/bin/python2
+sudo ldconfig
