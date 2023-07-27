88f773a7b2ad93b344739bded3edfafc8bf706c3
LoBrol <67478509+LoBrol@users.noreply.github.com>
2023-07-27 22:09:01 +0200

    Update pip2.sh

--- pip2.sh
+++ pip2.sh
@@ -2,5 +2,6 @@
 
 
 
-curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py --no-check-certificate
-python2.7 get-pip.py
+wget https://files.pythonhosted.org/packages/fd/76/3c7f726ed5c582019937f178d7478ce62716b7e8263344f1684cbe11ab3e/setuptools-45.0.0.zip
+python2 ./setuptools-44.1.1/setup.py install
+
