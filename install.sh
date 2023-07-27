f06e549b62bb5bd90028a0401bda56033cfaeff6
LoBrol <67478509+LoBrol@users.noreply.github.com>
2023-07-27 13:20:47 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ sudo apt install -y build-essential
 sudo apt install -y openssl
 # sudo apt install -y libssl-dev
 # sudo apt install -y zlib1g-dev
-sudo apt install -y libreadline-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libncurses5-dev libnss3-dev libffi-dev zlib1g-dev
+sudo apt install -y libreadline-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libncurses5-dev libnss3-dev libffi-dev zlib1g-dev libntirpc-dev
 sudo apt install -y gcc
 sudo apt install -y wget
 sudo apt install -y curl
@@ -21,8 +21,10 @@ sudo apt install -y make
 wget https://www.python.org/ftp/python/2.7.9/Python-2.7.9.tgz
 sudo tar xzf Python-2.7.9.tgz
 cd Python-2.7.9
-sudo ./configure --enable-optimization
-sudo make altinstall
+./configure --enable-optimizations --includedir=/usr/include/tirpc/
+#sudo make altinstall
+sudo make
+sudo make install
 alias python2='python2.7' >> .bashrc
 source ~/.bashrc
 
