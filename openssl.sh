e2789563a6ef0f21bcf73d35a63c9f838f355d32
LoBrol <67478509+LoBrol@users.noreply.github.com>
2023-07-27 19:27:39 +0200

    Update openssl.sh

--- openssl.sh
+++ openssl.sh
@@ -8,11 +8,11 @@ cd openssl-1.1.1g
 ./config --prefix=/home/ubuntu/openssl --openssldir=/home/ubuntu/openssl no-ssl2
 make
 sudo make install
-cd ~
 
-echo export PATH=$HOME/openssl/bin:$PATH >> .bashrc
-echo export LD_LIBRARY_PATH=$HOME/openssl/lib >> .bashrc
-echo export LC_ALL="en_US.UTF-8" >> .bashrc
-echo export LDFLAGS="-L /home/ubuntu/openssl/lib -Wl,-rpath,/home/ubuntu/openssl/lib" >> .bashrc
-source .bashrc
-./.bashrc
+# cd ~
+# echo export PATH=$HOME/openssl/bin:$PATH >> .bashrc
+# echo export LD_LIBRARY_PATH=$HOME/openssl/lib >> .bashrc
+# echo export LC_ALL="en_US.UTF-8" >> .bashrc
+# echo export LDFLAGS="-L /home/ubuntu/openssl/lib -Wl,-rpath,/home/ubuntu/openssl/lib" >> .bashrc
+# source .bashrc
+# ./.bashrc
