16128d78c3470197668084300e8a22def5d53688
LoBrol <67478509+LoBrol@users.noreply.github.com>
2023-07-27 19:50:22 +0200

    Update python2.7.sh

--- python2.7.sh
+++ python2.7.sh
@@ -7,6 +7,8 @@ tar xzf Python-2.7.9.tgz
 cd Python-2.7.9
 ./configure --enable-optimizations --includedir=/usr/include/tirpc/
 make altinstall
-cd ~
-echo alias python2='python2.7' >> .bashrc
-source ~/.bashrc
+
+
+# cd ~
+# echo alias python2='python2.7' >> .bashrc
+# source ~/.bashrc
