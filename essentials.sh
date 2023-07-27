093e55e088a485de04f0adc6b6ea8c6f70d80b93
LoBrol <67478509+LoBrol@users.noreply.github.com>
2023-07-27 18:56:53 +0200

    Update essentials.sh

--- essentials.sh
+++ essentials.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 
+
 sudo apt update
 
 
-# ----- INSTALLING NECESSARY PROGRAMS/LIBRARIES ---------------------
 sudo apt install -y build-essential
 # sudo apt install -y openssl
 sudo apt install -y libreadline-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libncurses5-dev libnss3-dev libffi-dev zlib1g-dev libntirpc-dev
