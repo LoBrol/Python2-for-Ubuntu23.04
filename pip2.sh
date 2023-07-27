fceba123ab5425d79c06592c57818254d3fdacc1
LoBrol <67478509+LoBrol@users.noreply.github.com>
2023-07-27 22:29:23 +0200

    Update pip2.sh

--- pip2.sh
+++ pip2.sh
@@ -6,7 +6,7 @@ wget https://files.pythonhosted.org/packages/fd/76/3c7f726ed5c582019937f178d7478
 unzip setuptools-45.0.0.zip
 sudo python2 ./setuptools-44.1.1/setup.py install
 
-mkdir pip
-cd pip
+mkdir wheels
 wget https://files.pythonhosted.org/packages/27/79/8a850fe3496446ff0d584327ae44e7500daf6764ca1a382d2d02789accf7/pip-20.3.4-py2.py3-none-any.whl --no-check-certificate
-python2 ./pip-20.3.4-py2.py3-none-any.whl/pip install --no-index --find-links ./wheels/ pip --ignore-installed
+mv pip-20.3.4-py2.py3-none-any.whl wheels
+sudo python2 ./wheels/pip-20.3.4-py2.py3-none-any.whl/pip install --no-index --find-links ./wheels/ pip --ignore-installed
