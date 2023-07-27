cfeadf8e4a6c7b3d84ae65b68059e5320a0a3f95
LoBrol <67478509+LoBrol@users.noreply.github.com>
2023-07-27 18:56:12 +0200

    Create python2.7.sh

---
+++ python2.7.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+
+
+wget https://www.python.org/ftp/python/2.7.9/Python-2.7.9.tgz
+tar xzf Python-2.7.9.tgz
+cd Python-2.7.9
+./configure --enable-optimizations --includedir=/usr/include/tirpc/
+make altinstall
+cd ~
+echo alias python2='python2.7' >> .bashrc
+source ~/.bashrc
