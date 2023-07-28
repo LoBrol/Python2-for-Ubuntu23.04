8deb821ecee87c5a0e74d9d02703ae6d06abed35
LoBrol <67478509+LoBrol@users.noreply.github.com>
2023-07-28 23:56:53 +0200

    Delete sussy.sh

--- sussy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-
-export LDFLAGS="-L/path/to/openssl/lib/ -L/path/to/openssl/lib64/"
-export LD_LIBRARY_PATH="/path/to/openssl/lib/:/path/to/openssl/lib64/"
-export CPPFLAGS="-I/path/to/openssl/include -I/path/to/openssl/include/openssl"
-./configure #--prefix=/path/to/custom/python/
-make
-make install
