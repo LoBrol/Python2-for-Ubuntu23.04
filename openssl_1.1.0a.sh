68cf5c2925160927b54b05918300df2baa0c0fb6
LoBrol <67478509+LoBrol@users.noreply.github.com>
2023-07-28 18:02:45 +0200

    Create openssl_1.1.0a.sh

---
+++ openssl_1.1.0a.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+wget https://ftp.openssl.org/source/old/1.1.0/openssl-1.1.0a.tar.gz
+tar xvf openssl-1.1.0a.tar.gz
+cd openssl-1.1.0a
+./config
+make
+sudo make install
+
+sudo rm -rf /usr/bin/openssl
+sudo ln -sf /usr/local/ssl/bin/openssl /usr/bin/openssl
+sudo ldconfig
