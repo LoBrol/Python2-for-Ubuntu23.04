e2c984b6241f6685f02c0e171d9c5eee2a8ff2df
LoBrol <67478509+LoBrol@users.noreply.github.com>
2023-07-28 23:39:20 +0200

    Update pyenv.sh

--- pyenv.sh
+++ pyenv.sh
@@ -9,3 +9,6 @@ curl https://pyenv.run | bash
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+
+cd ~
+source .bashrc
