fcac2d5df43ed6bfd45159f64cffbd019bcc5158
LoBrol <67478509+LoBrol@users.noreply.github.com>
2023-07-28 22:47:09 +0200

    Create sussy2.sh

---
+++ sussy2.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+
+
+
+
+# new openssl install
+curl https://www.openssl.org/source/openssl-1.1.1a.tar.gz
+tar xzf openssl-1.1.1a.tar.gz
+cd openssl-1.1.1a
+./config shared --prefix=/usr/local/
+make
+sudo make install
+
+
+
+
+
+# Python install script
+export LDFLAGS="-L/usr/local/lib/"
+export LD_LIBRARY_PATH="/usr/local/lib/"
+export CPPFLAGS="-I/usr/local/include -I/usr/local/include/openssl"
+
+apt-get update
+apt-get install build-essential checkinstall -y
+apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev -y
+
+cd /home/ubuntu/Download
+wget https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tgz
+tar xzf Python-2.7.18.tgz
+cd Python-2.7.18 
+./configure --prefix=/usr/local/ 
+make
+sudo make install
