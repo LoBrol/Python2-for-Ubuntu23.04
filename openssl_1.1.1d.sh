9eaab193b252c6fe60c477a401e7e575f2c6f1f1
LoBrol <67478509+LoBrol@users.noreply.github.com>
2023-07-28 23:06:07 +0200

    Create openssl_1.1.1d.sh

---
+++ openssl_1.1.1d.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+
+
+sudo apt remove openssl
+sudo rm -rf /usr/include/openssl
+
+wget https://ftp.openssl.org/source/old/1.1.1/openssl-1.1.1d.tar.gz --no-check-certificate
+tar zxvf openssl-1.1.1d.tar.gz
+cd openssl-1.1.1d
+./config
+make
+sudo make install
+
+
+sudo rm -rf /usr/bin/openssl
+sudo ln -s /usr/local/bin/openssl /usr/bin/openssl
+sudo ldconfig
