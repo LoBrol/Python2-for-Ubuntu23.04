ccd059eb17a7e680d8d58e9f1f1c8f504e8b625f
LoBrol <67478509+LoBrol@users.noreply.github.com>
2023-07-28 20:24:57 +0200

    Update essentials.sh

--- essentials.sh
+++ essentials.sh
@@ -7,7 +7,7 @@ sudo apt update
 
 sudo apt install -y build-essential
 sudo apt install -y software-properties-common
-sudo apt install -y openssl
+# sudo apt install -y openssl
 sudo apt install -y libreadline-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libncurses5-dev libnss3-dev libffi-dev zlib1g-dev libntirpc-dev
 sudo apt install -y gcc
 sudo apt install -y wget
