918aaa449db77f20919c54c3cbc6445e72e20983
LoBrol <67478509+LoBrol@users.noreply.github.com>
2023-07-29 12:39:30 +0200

    Update pyenv.sh

--- pyenv.sh
+++ pyenv.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
 
-sudo apt update
-sudo apt install -y curl
-sudo apt install -y git
 
 curl https://pyenv.run | bash
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
 
 . ~/.bashrc
