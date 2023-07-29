645c00405819500b3b65dc331c8e4af6b1d464cf
LoBrol <67478509+LoBrol@users.noreply.github.com>
2023-07-29 13:07:41 +0200

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -2,15 +2,15 @@
 
 
 
-sudo apt update
-sudo apt install -y build-essential
-sudo apt install -y git
-
+# ----- INSTALLING BREW ---------------------------------------------------------------------------
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/$USER/.bashrc
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
+
+# ----- INSTALLING PYTHON -------------------------------------------------------------------------
 brew install pyenv
 pyenv install 2.7.18
 pyenv global 2.7.18
 PATH=$(pyenv root)/shims:$PATH
+pip install --upgrade pip
